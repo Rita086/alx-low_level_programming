81e3cbe3e53ef1dbe84cf3c23d3d6f5b389c863d
Rita086 <gathogoritawanjiru@gmail.com>
2023-06-20 05:59:49 -0700

    My file 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet - Print the alphabet
+* Return: void
+*/
+void print_alphabet(void)
+{
+        char c;
+	int k = 0;
+
+	while (k <= 0)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{	
+        _putchar(c);
+	}
+        _putchar('\n');
+	k++;
+	}
+}
