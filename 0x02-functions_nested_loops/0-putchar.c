54d41cb02d8fc34685d1974abed52462565adc79
Rita086 <gathogoritawanjiru@gmail.com>
2023-06-20 04:50:13 -0700

    My file  0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main -Entry point
+*
+* * Return: 0
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('j');
+	_putchar('l');
+	_putchar('u');
+	_putchar('c');
+	_putchar('r');
+	_putchar('t');
+	_putchar('\n');
+	 return (0);
+}
