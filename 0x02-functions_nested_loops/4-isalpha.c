9c64ad1d1a400d91f1a7df31692ba2ca3503cb83
Rita086 <gathogoritawanjiru@gmail.com>
2023-06-21 01:36:41 -0700

    My file ./4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,9 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 98) || ((c >= 97 c <=)
-		{
-		return (1);
-		}
+	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
 	return (0);
 }
-
-
-
