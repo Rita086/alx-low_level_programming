119e08ae491d469386c61569aa1e47dea288f1fd
Rita086 <gathogoritawanjiru@gmail.com>
2023-06-22 03:12:15 -0700

    My file 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letter
+ * @y: check the int
+ * Return: 1 for upper letter, otherwise 0
+ */
+int _isupper(int y)
+{
+	if(y >= 65 && y <= 98)
+	{
+	return (1);
+	}
+	return (0);
+}
