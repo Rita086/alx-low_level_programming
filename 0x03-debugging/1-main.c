625ca0d5deee97074d93532db3d85e8989317633
Rita086 <gathogoritawanjiru@gmail.com>
2023-06-22 06:56:55 -0700

    My file 1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - An infinite loop is generated
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	printf("Infinite loop incoming :|\n");
+	a = 0;
+	/*while (a < 10)*/
+	/*{*/
+	/*  putchar(a)*/
+	/*}*/
+	printf("Infinite loop avoided! \\0/\n");
+	return (0);
+}
