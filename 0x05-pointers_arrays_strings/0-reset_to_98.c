0065223ce3224e2f483f8211f2d232cb043bdc7d
Rita086 <gathogoritawanjiru@gmail.com>
2023-06-27 15:05:11 -0700

    My file 8-print_array.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int and updates
  * it points the value to 98
- * @*n - an int input pointer
+ * @n - an int input pointer
  * Return:nothing
  */
 void reset_to_98(int *n)
