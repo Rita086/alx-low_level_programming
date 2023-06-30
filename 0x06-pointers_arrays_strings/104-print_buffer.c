68a5f4b7df7c5af637757694a4b9e8eeb71cd468
Rita086 <gathogoritawanjiru@gmail.com>
2023-06-30 03:44:10 -0700

    M file 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,98 +1,50 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
 /**
-
- * print_buffer - prints buffer
-
+ * print_buffer - buffer is printed
  * @b: buffer
-
  * @size: size
-
  * Return: void
-
  */
 
-
 void print_buffer(char *b, int size)
-
 {
+	int o, l, k;
 
-        int o, j, i;
+	o = 0;
 
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+	l = size - o < 10 ? size - o : 10;
+	printf("%08x: ", o);
+	for (k = 0; k < 10; k++)
+	{
+	if (k < l)
+	printf("%02x", *(b + o + k));
+	else
+	printf("  ");
+	if (k % 2)
+	{
+	printf(" ");
+	}
+	}
+	for (k = 0; k < l; k++)
+	{
+	int c = *(b + o + k);
 
-        o = 0;
-
-
-        if (size <= 0)
-
-        {
-
-                printf("\n");
-
-                return;
-
-        }
-
-        while (o < size)
-
-        {
-
-                j = size - o < 10 ? size - o : 10;
-
-                printf("%08x: ", o);
-
-                for (i = 0; i < 10; i++)
-
-                {
-
-                        if (i < j)
-
-                                printf("%02x", *(b + o + i));
-
-                        else
-
-                                printf("  ");
-
-                        if (i % 2)
-
-                        {
-
-                                printf(" ");
-
-                        }
-
-                }
-
-                for (i = 0; i < j; i++)
-
-                {
-
-                        int c = *(b + o + i);
-
-
-                        if (c < 32 || c > 132)
-
-                        {
-
-                                c = '.';
-
-                        }
-
-                        printf("%c", c);
-
-                }
-
-                printf("\n");
-
-                o += 10;
-
-        }
-
+	if (c < 32 || c > 132)
+	{
+	c = '.';
+	}
+	printf("%c", c);
+	}
+	printf("\n");
+	o += 10;
+	}
 }
-
-
-
-
