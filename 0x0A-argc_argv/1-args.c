8a8bef29d3b0c46ca2b7333d183440b4ab23535f
Rita086 <gathogoritawanjiru@gmail.com>
2023-07-07 07:29:51 -0700

    My file 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main -  the number of arguments passed to the program to be outputed
+ * @argc: number command line of arguments
+ * @argv: array containing the program command line arguments
  *
  * Return: Always 0 (Success)
  */
