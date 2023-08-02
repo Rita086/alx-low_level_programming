def7c7130682923a29589d2e331e3097c550e594
Rita086 <gathogoritawanjiru@gmail.com>
2023-08-02 13:13:32 -0700

    My file 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2 - a linked list is freed
+ * @head: indicator to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	
+
 	if (head == NULL)
 	return;
 	while (*head)
