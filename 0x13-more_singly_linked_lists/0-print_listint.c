a9e485762f4bec78dca535027573a01de87d2a77
Rita086 <gathogoritawanjiru@gmail.com>
2023-08-02 12:09:58 -0700

    My file 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - all the elements in a linked list are printed
+ * @h: linked list of type listint_t to be printed
+ * Return: amount of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+	}
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is my readme file in this directory 0x13-more_singly_linked_lists
