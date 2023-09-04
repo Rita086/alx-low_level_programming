d485eba808a98c6117281fedbef678decd686596
root <your.email@example.com>
2023-09-04 01:59:57 -0700

    This file  0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,8 +5,7 @@
  * read_textfile- Read text file  to be output in STDOUT.
  * @filename: text file being gone through
  * @letters: numb of letters to be gone through
- * Return: w- actual number of bytes read and outputed 
- * otherwise 0 or filename is NULL.
+ *  Return: w- actual number of bytes read and outputed otherwise 0 or null
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -18,9 +17,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	td = open(filename, O_RDONLY);
 	if (td == -1)
 	return (0);
-	buf = malloc(sizeof(char) * letters);
-	y = read(td, buf, letters);
-	x = write(STDOUT_FILENO, buf, y);
+	str = malloc(sizeof(char) * letters);
+	y = read(td, str, letters);
+	x = write(STDOUT_FILENO, str, y);
 	free(str);
 	close(td);
 	return (x);
