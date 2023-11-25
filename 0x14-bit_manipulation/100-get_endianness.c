51f0e6597f6eba5258a7c75569d7a289014ce905
Rita086 <gathogoritawanjiru@gmail.com>
2023-11-24 18:50:55 -0800

    My sevenenth commit in this file

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_endianness - looks if a machine is little or big endian
+ * Return: 0 for big, 1 for small
+ */
+int get_endianness(void)
+{
+	unsigned int m = 1;
+	char *c = (char *) &m;
+	return (*c);
+}
