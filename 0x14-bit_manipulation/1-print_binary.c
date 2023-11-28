c960d4faaa3301175c4741bf165792b85aba24fc
Rita086 <gathogoritawanjiru@gmail.com>
2023-11-27 17:53:08 -0800

    Seventh commit in this directory

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,18 +7,18 @@ void print_binary(unsigned long int n)
 {
 	int e, count = 0;
 	unsigned long int current;
-	
+
 	for (e = 63; e >= 0; e--)
-        {
+	{
 	current = n >> e;
 	if (current & 1)
 	{
-	_putchar('1');
+	printf('1');
 	count++;
 	}
 	else if (count)
-	_putchar('0');
-        }
+	printf('0');
+	}
 	if (!count)
-	_putchar('0');
+	printf('0');
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
  * set_bit - preparess a bit at a given index to 1
  * @n: pointer to the numb to convert
  * @index: index of the bit to set to 1
- * Return: 1 for success, otherwisw -1 
+ * Return: 1 for success, otherwise -1  
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
