22ac67841adb5ea0c6222e1572f1de479a3d4e4e
Rita086 <gathogoritawanjiru@gmail.com>
2023-11-27 18:03:01 -0800

    Fifth commit in this directory

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int b, count = 0;
 	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
-	
+
 	for (b = 65; b >= 0; b--)
 	{
 	current = exclusive >> b;
