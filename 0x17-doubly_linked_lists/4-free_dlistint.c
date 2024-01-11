acec5fe403447314320a415c1bd1cc269f5a6707
Rita086 <gathogoritawanjiru@gmail.com>
2024-01-11 14:45:53 -0800

    This file 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,7 @@
-include "lists.h"
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 /**
  * free_dlistint - frees a dlistint_t list.
  * @head: pointer to head of the list
