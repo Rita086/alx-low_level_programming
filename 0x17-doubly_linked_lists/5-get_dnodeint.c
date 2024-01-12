8fb6c1a4fc966872c059bdaf2ca8d04d3cbb3d4a
Rita086 <gathogoritawanjiru@gmail.com>
2024-01-12 03:30:05 -0800

    This file 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,23 +7,23 @@
  * @head: pointer to head of list
  * @index: index of the node to search for, starting from 0
  * Return: nothing node or null
- **/
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-        unsigned int len;
-        dlistint_t *tmp;
+	unsigned int len;
+	dlistint_t *tmp;
 
-        len = 0;
-        if (head == NULL)
-        return (NULL);
+	len = 0;
+	if (head == NULL)
+		return (NULL);
 
-        tmp = head;
-        while (tmp)
-        {
-        if (index == len)
-        return (tmp);
-        len++;
-        tmp = tmp->next;
-        }
-        return (NULL);
+	tmp = head;
+	while (tmp)
+	{
+		if (index == len)
+			return (tmp);
+		len++;
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
