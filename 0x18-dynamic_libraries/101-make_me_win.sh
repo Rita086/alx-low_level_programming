e87aa7c315aef05eca0a680e9dcae7584408f965
Rita086 <gathogoritawanjiru@gmail.com>
2024-01-19 04:09:25 -0800

    This file mutilate.c

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-wget -p .. https://raw.githubusercontent.com/Rita086.alx-low_level_programming/master/0x18-dynamic_libraries/libmutilate.so
+wget -p .. https://raw.githubusercontent.com/Rita086/alx-low_level_programming/master/0x18-dynamic_libraries/libmutilate.so
 export LD_PRELOAD="$PWD/../libmutilate.so"
